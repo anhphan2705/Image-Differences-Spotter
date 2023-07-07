8c7f2d2dabcb16a6e4f6a4c9e12d2af9277a2e40
Anh Phan <anhphanvt2705@gmail.com>
2023-07-07 09:38:32 +0700

    add comment to how imagechops work

--- image_difference_pillow/image_difference_pillow.py
+++ image_difference_pillow/image_difference_pillow.py
@@ -3,6 +3,11 @@ from PIL import Image, ImageChops
 img1 = Image.open("./images/1.jpg")
 img2 = Image.open("./images/2.jpg")
 
+""" How ImageChops.difference works
+    - ImageChops.difference computes the 'absolute value of the pixel-by-pixel difference between the two images'
+    - Results in a difference image that is returned.
+"""
+
 # Main
 diff = ImageChops.difference(img1, img2)
 # Output
