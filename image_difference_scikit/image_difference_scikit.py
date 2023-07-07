9253e1abba75ec96fb0f4d336025350b6c03a86a
Anh Phan <anhphanvt2705@gmail.com>
2023-07-07 16:03:38 +0700

    upgrade for cam image, need improvement

--- image_difference_scikit/image_difference_scikit.py
+++ image_difference_scikit/image_difference_scikit.py
@@ -14,6 +14,18 @@ def show_image(header, image):
 def write_image(directory, image):
     print("[Console] Saving image")
     cv2.imwrite(directory, image)
+    
+# def resize_image(image, scale_percent):
+#     print("[Console] Resizing image")
+#     width = int(image.shape[1] * scale_percent / 100)
+#     height = int(image.shape[0] * scale_percent / 100)
+#     dim = (width, height)
+#     return dim, cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+
+def resize_image(image, height, width):
+    print("[Console] Resizing image to 720p")
+    dim = (height, width)
+    return dim, cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
 
 def convert_to_gray(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -48,60 +60,66 @@ def get_structural_simlarity(first_image, second_image):
     print("[Console] Similarity score of {:.4f}%".format(score * 100))
     return diff_img
 
-def get_diff_mask(image, diff_image):
+def get_diff_mask(image, diff_image, minDiffArea):
     mask = np.zeros(image.shape, dtype='uint8')
     contours = get_contours(diff_image)
     # Multiple objects in a contours
     for c in contours:
         area = cv2.contourArea(c)
-        # For any object that has a contour area > 40 pixels
-        if area > 40:
+        # For any object that has a contour area > minDiffArea (40 pixel for smaller details)
+        if area > minDiffArea:
             # Mark it
             x,y,w,h = cv2.boundingRect(c)
             cv2.drawContours(mask, [c], 0, (255,255,255), -1)
     return mask
 
-def get_diff_rect(image, diff_image):
+def get_diff_rect(image, diff_image, minDiffArea):
     print("[Console] Drawing rectangle around the differences")
     img = image.copy()
     contours = get_contours(diff_image)
     # Multiple objects in a contours
     for c in contours:
         area = cv2.contourArea(c)
-        # For any object that has a contour area > 40 pixels
-        if area > 40:
+        # For any object that has a contour area > minDiffArea (40 pixel for smaller details)
+        if area > minDiffArea:
             # Mark it
             x,y,w,h = cv2.boundingRect(c)
             cv2.rectangle(img, (x, y), (x + w, y + h), (36,255,12), 2)
     return img
             
-def get_diff_filled(image, diff_image):
+def get_diff_filled(image, diff_image, minDiffArea):
     contours = get_contours(diff_image)
     # Multiple objects in a contours
     for c in contours:
         area = cv2.contourArea(c)
-        # For any object that has a contour area > 40 pixels
-        if area > 40:
+        # For any object that has a contour area > minDiffArea (40 pixel for smaller details)
+        if area > minDiffArea:
             # Mark it
             x,y,w,h = cv2.boundingRect(c)
             cv2.drawContours(image, [c], 0, (0,255,0), -1)
     return image
         
 # Main
-first_img = get_image("./images/1.jpg")
-second_img = get_image("./images/2.jpg")
+first_img = get_image("./images/real/1.jpg")
+second_img = get_image("./images/real/2.jpg")
+first_img = resize_image(first_img, 1280, 720)[1]               # Resize for real camera application, otherwise not needed
+second_img = resize_image(second_img, 1280, 720)[1]             # Resize for real camera application, otherwise not needed
+
 diff_img = get_structural_simlarity(first_img, second_img)
       
-first_rect = get_diff_rect(first_img, diff_img)
-second_rect = get_diff_rect(second_img, diff_img)
-mask = get_diff_mask(first_img, diff_img)
-filled_img = get_diff_filled(second_img ,diff_img)
+first_rect = get_diff_rect(first_img, diff_img, 750)
+second_rect = get_diff_rect(second_img, diff_img, 750)
+# mask = get_diff_mask(first_img, diff_img, 500)
+filled_img = get_diff_filled(second_img ,diff_img, 750)
 
 # Output
 show_image('before', first_rect)
+write_image("./output/real/1_2/first_rect.jpg", first_rect)
 show_image('after', second_rect)
-show_image('diff', diff_img)
-show_image('mask', mask)
-show_image('filled after', filled_img)
-
-
+write_image("./output/real/1_2/second_rect.jpg", second_rect)
+# show_image('diff', diff_img)
+# write_image("./output/test/diff_img.jpg", diff_img)
+# show_image('mask', mask)
+# write_image("./output/real/2_3/mask.jpg", mask)
+show_image('filled', filled_img)
+write_image("./output/real/1_2/filled.jpg", filled_img)
