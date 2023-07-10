874193e759e33159511db9867d11d92d0b841a9a
Anh Phan <anhphanvt2705@gmail.com>
2023-07-10 10:09:46 +0700

    update image data for testing

--- image_difference_absdiff/image_difference_absdiff.py
+++ image_difference_absdiff/image_difference_absdiff.py
@@ -1,8 +1,8 @@
 import cv2
 
 # Load images as grayscale
-image1 = cv2.imread("./images/1.jpg", 0)
-image2 = cv2.imread("./images/2.jpg", 0)
+image1 = cv2.imread("./images/real/1.jpg", 0)
+image2 = cv2.imread("./images/real/1_special.jpg", 0)
 
 # Calculate the per-element absolute difference between 
 # two arrays or between an array and a scalar
